8ecc7b36fdc48fdc74120be3b127e504c944566d
Roman-Daher <84515748+RomanBallinFaxJah@users.noreply.github.com>
2021-06-11 10:45:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementById('button').addEventListener('click', multiply)
-answer = 0
+let answer = 0
 
 function multiply () {
-  number1 = parseInt(document.getElementById('number1').value)
-  number2 = parseInt(document.getElementById('number2').value)
+  let number1 = parseInt(document.getElementById('number1').value)
+  let number2 = parseInt(document.getElementById('number2').value)
   for (let i = 0; i < number1; i++) {
-  answer += number2
+    answer += number2
   }
   document.getElementById('answer').innerHTML = answer
 }
