7b465808b1bdea92f35436f47ee059e933754a66
Roman-Daher <84515748+RomanBallinFaxJah@users.noreply.github.com>
2021-06-11 10:48:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ document.getElementById('button').addEventListener('click', multiply)
 let answer = 0
 
 function multiply () {
-  let number1 = parseInt(document.getElementById('number1').value)
-  let number2 = parseInt(document.getElementById('number2').value)
+  const number1 = parseInt(document.getElementById('number1').value)
+  const number2 = parseInt(document.getElementById('number2').value)
   for (let i = 0; i < number1; i++) {
     answer += number2
   }
